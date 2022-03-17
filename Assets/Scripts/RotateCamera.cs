b672105d1b7784d054805950ef1fe51fc8f223f6
vinojko <vinojko@gmail.com>
2022-03-17 17:28:41 +0100

    RotateCamera Mobile

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -5,11 +5,43 @@ using UnityEngine;
 public class RotateCamera : MonoBehaviour
 {
 
-    public float Speed = 5;
+    private Touch initTouch = new Touch();
+    public Camera cam;
+
+    private float rotX = 0f;
+    private float rotY = 0f;
+    private Vector3 origRot;
+
+    public float rotSpeed = 0.5f;
+    public float dir = -1;
     // Start is called before the first frame update
     // Update is called once per frame
-    void Update()
+
+    void Start(){
+        origRot = cam.transform.eulerAngles;
+        rotX = origRot.x;
+        rotY = origRot.y;
+
+    }
+    void FixedUpdate()
     {
-        transform.eulerAngles += Speed * new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"),0);
+        foreach(Touch touch in Input.touches){
+            if(touch.phase == TouchPhase.Began){
+                initTouch = touch;
+            }
+            else if(touch.phase == TouchPhase.Moved){
+                float deltaX = initTouch.position.x - touch.position.x;
+                float deltaY = initTouch.position.y - touch.position.y;
+
+                rotX -= deltaX * Time.deltaTime * rotSpeed*dir;
+                rotY += deltaY * Time.deltaTime * rotSpeed*dir;
+
+                cam.transform.eulerAngles = new Vector3(rotX, rotY,0f);
+
+            }
+            else if(touch.phase == TouchPhase.Ended){
+                initTouch = new Touch();
+            }
+        }
     }
 }
