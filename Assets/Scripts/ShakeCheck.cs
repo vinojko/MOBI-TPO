a1e2a2ca15cf5f7088c59d95607a858b824bcce6
vinojko <vinojko@gmail.com>
2022-03-18 15:07:16 +0100

    MainMenu

--- Assets/Scripts/ShakeCheck.cs
+++ Assets/Scripts/ShakeCheck.cs
@@ -6,11 +6,8 @@ public class ShakeCheck : MonoBehaviour
 {
 
     float accelerometerUpdateInterval = 1.0f / 60.0f;
-    // The greater the value of LowPassKernelWidthInSeconds, the slower the
-    // filtered value will converge towards current input sample (and vice versa).
     float lowPassKernelWidthInSeconds = 1.0f;
-    // This next parameter is initialized to 2.0 per Apple's recommendation,
-    // or at least according to Brady! ;)
+
     float shakeDetectionThreshold = 2.0f;
 
     float lowPassFilterFactor;
