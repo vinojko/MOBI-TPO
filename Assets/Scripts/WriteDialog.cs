7856b45efe3e32ba01b806f98dbc5049274ada49
vinojko <vinojko@gmail.com>
2022-04-10 19:48:36 +0200

    Writedialog

--- Assets/Scripts/WriteDialog.cs
+++ Assets/Scripts/WriteDialog.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class WriteDialog : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public TextMeshProUGUI Dialog;
+    private int Index = 0;
+    public float DialogSpeed;
+
+    public void NextSentence(string Sentence)
     {
+   
+        StartCoroutine(Write(Sentence));
         
     }
-
-    // Update is called once per frame
-    void Update()
+    IEnumerator Write(string Sentence)
     {
+
+        foreach (char Character in Sentence.ToCharArray())
+        {
+            Dialog.text += Character;
+            yield return new WaitForSeconds(DialogSpeed);
+        }
+        Index++;
         
+     
     }
 }
