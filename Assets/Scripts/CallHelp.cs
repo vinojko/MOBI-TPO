a2cfe2057cee05e9236b3c99ae67331a764c5ccd
vinojko <vinojko@gmail.com>
2022-06-07 00:57:13 +0200

    Micui za 3 sobo, main menu fix, CallHelp skripta

--- Assets/Scripts/CallHelp.cs
+++ Assets/Scripts/CallHelp.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class CallHelp : MonoBehaviour
 {
-    public DialogTrigger dialog1;
+    public DialogTrigger dialog1, dialog2;
+    public GameObject Answers;
+    public GameObject MicUI;
     void Awake()
     {
         GameManager.OnGameStateChanged += GameManagerOnStateChanged;
@@ -34,5 +36,24 @@ public class CallHelp : MonoBehaviour
     private IEnumerator ShowAnswers()
     {
         yield return new WaitForSeconds(1f);
+        LeanTween.moveLocal(Answers, new Vector3(0f, -50f, 0f), 1.5f).setEaseInOutExpo();
+
+    }
+
+    public void AnswerRight()
+    {
+        LeanTween.moveLocal(Answers, new Vector3(0f, -1035f, 0f), 1.5f).setEaseInOutExpo();
+        StartAnimation();
+        dialog2.TriggerDialog();
+
+    }
+    private void StartAnimation()
+    {
+        LeanTween.moveLocal(MicUI, new Vector3(-300f, -850f, 0f), 2.2f).setEaseInOutExpo();
+    }
+
+    private void EndAnimation()
+    {
+        LeanTween.moveLocal(MicUI, new Vector3(-1200f, -850f, 0f), 1.7f).setDelay(0.2f).setEase(LeanTweenType.easeOutElastic);
     }
 }
