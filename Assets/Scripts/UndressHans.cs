5428d13ea4d5363d13c90dc3c64862ce06b8302c
vinojko@gmail.com <vinojko@gmail.com>
2023-01-03 00:34:52 +0100

    b

--- Assets/Scripts/UndressHans.cs
+++ Assets/Scripts/UndressHans.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class UndressHans : MonoBehaviour
 {
 
-    public GameObject HansDressed, HansUndressed;
+    public GameObject HansDressed, HansUndressed, nipples;
     [SerializeField]
     DialogTrigger undressHanzDialog;
     // Start is called before the first frame update
     void Awake()
     {
         GameManager.OnGameStateChanged += GameManagerOnStateChanged;
+        nipples.SetActive(false);
     }
 
 
@@ -37,6 +38,7 @@ public class UndressHans : MonoBehaviour
         {
             HansDressed.SetActive(false);
             HansUndressed.SetActive(true);
+            nipples.SetActive(true);
 
             GameManager.instance.UpdateGameState(GameState.ChinLift);
         }
